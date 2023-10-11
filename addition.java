468bd227259e3e1b04f7630a5dacf51aa7fc8647
Jaybhaye Sachin <sachinjaybhaye931@gmail.com>
2023-10-11 12:04:35 +0530

    Addition program is added.

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        int c= a+b;
+        System.out.println("Addition is :" +c);
+    }
+}
