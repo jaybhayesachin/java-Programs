c56c946aeae4b6d043dd7a749f19d23afe5af41a
Jaybhaye Sachin <sachinjaybhaye931@gmail.com>
2023-10-15 13:41:42 +0530

    One file add

---
+++ loops_program/Dowhile.java
@@ -0,0 +1,12 @@
+package loops_program;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+    }
+}
